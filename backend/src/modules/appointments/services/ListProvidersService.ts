14514c2a4d4080e3c0b2363a1697fd63db10c017
Raul Neto <raulneto90@gmail.com>
2021-02-01 15:46:42 -0300

    Configuring cache of providers

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'tsyringe';
-import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   userId: string;
@@ -12,12 +12,23 @@ export default class ListProviderService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) { }
 
   public async execute({ userId }: IRequest): Promise<User[]> {
-    const users = await this.usersRepository.findAllProviders({
-      exceptUserId: userId,
-    });
+    let users = await this.cacheProvider.recover<User[]>(
+      `providers-list:${userId}`,
+    );
+
+    if (!users) {
+      users = await this.usersRepository.findAllProviders({
+        exceptUserId: userId,
+      });
+    }
+
+    await this.cacheProvider.save(`providers-list:${userId}`, users);
 
     return users;
   }
