53c246d77946ede426dcce20ccaed8eb3bddf63c
Raul Neto <raulneto90@gmail.com>
2021-02-11 08:49:37 -0300

    Finished password recovery page

--- web/src/components/ToastContainer/Toast/index.tsx
+++ web/src/components/ToastContainer/Toast/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+
 import {
   FiAlertCircle,
   FiCheckCircle,
@@ -12,12 +13,15 @@ import { Container } from './styles';
 
 interface ToastProps {
   message: ToastMessage;
+
   style: object;
 }
 
 const icons = {
   info: <FiInfo size={24} />,
+
   success: <FiCheckCircle size={24} />,
+
   error: <FiAlertCircle size={24} />,
 };
 
@@ -41,8 +45,10 @@ const Toast: React.FC<ToastProps> = ({ message, style }) => {
       style={style}
     >
       {icons[message.type || 'info']}
+
       <div>
         <strong>{message.title}</strong>
+
         {message.description && <p>{message.description}</p>}
       </div>
 
---
+++ web/src/pages/ResetPassword/index.tsx
@@ -0,0 +1,119 @@
+import React, { useCallback, useRef } from 'react';
+
+import { FiLock } from 'react-icons/fi';
+
+import { useHistory, useLocation } from 'react-router-dom';
+
+import { Form } from '@unform/web';
+
+import { FormHandles } from '@unform/core';
+
+import * as Yup from 'yup';
+
+import { useToast } from '../../hooks/Toast';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+import api from '../../services/api';
+
+interface ResetPasswordFormData {
+  password: string;
+  password_confirmation: string;
+}
+
+const ResetPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleSubmit = useCallback(
+    async (data: ResetPasswordFormData) => {
+      formRef.current?.setErrors({});
+
+      try {
+        const schema = Yup.object().shape({
+          password: Yup.string().required('Campo obrigatório'),
+
+          password_confirmation: Yup.string().oneOf(
+            [Yup.ref('password'), null],
+            'Senhas não coincidem',
+          ),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('password/reset', {
+          password: data.password,
+          passwordConfirmation: data.password_confirmation,
+          token,
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro ao resetar senha',
+          description: 'Ocorreu um erro ao resetar sua senha. Tente novamente.',
+        });
+      }
+    },
+
+    [addToast, history, location.search],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="Gobarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Resete sua senha</h1>
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Nova senha"
+          />
+
+          <Input
+            name="password_confirmation"
+            icon={FiLock}
+            type="password"
+            placeholder="Confirme sua senha"
+          />
+
+          <Button type="submit">Alterar senha</Button>
+        </Form>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
+
+export default ResetPassword;
